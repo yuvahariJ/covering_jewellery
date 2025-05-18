5b64afb7e7118201633a9c9fb8af36f624a455d6
Yuvahari JR <yuvahari.j@gmail.com>
2025-05-18 17:45:34 +0530

    Updates

---
+++ _next/static/chunks/app/layout-f1d9bfe8f2872edd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},2916:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,347,23)),Promise.resolve().then(n.t.bind(n,9732,23))},9732:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[550,441,684,358],()=>s(2916)),_N_E=e.O()}]);
